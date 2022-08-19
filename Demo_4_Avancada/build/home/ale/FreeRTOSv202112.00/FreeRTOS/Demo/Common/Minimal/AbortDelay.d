bb9f732fb1f2bf3f53f22238bc5b9988b9b3e0d0
Aletronica <111475287+Aletronica@users.noreply.github.com>
2022-08-19 00:27:59 -0300

    Add files via upload

---
+++ Demo_4_Avancada/build/home/ale/FreeRTOSv202112.00/FreeRTOS/Demo/Common/Minimal/AbortDelay.d
@@ -0,0 +1,26 @@
+build//home/ale/FreeRTOSv202112.00/FreeRTOS/Demo/Common/Minimal/AbortDelay.o: \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Demo/Common/Minimal/AbortDelay.c \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/FreeRTOS.h \
+ FreeRTOSConfig.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcRecorder.h \
+ trcConfig.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcPortDefines.h \
+ trcSnapshotConfig.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcPortDefines.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcHardwarePort.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcKernelPort.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/projdefs.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/portable.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/deprecated_definitions.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/portable/ThirdParty/GCC/Posix/portmacro.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/mpu_wrappers.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/task.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/list.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/queue.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/task.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/semphr.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/queue.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/event_groups.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/timers.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/stream_buffer.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Demo/Common/include/AbortDelay.h
---
+++ Demo_4_Avancada/build/home/ale/FreeRTOSv202112.00/FreeRTOS/Demo/Common/Minimal/MessageBufferDemo.d
@@ -0,0 +1,21 @@
+build//home/ale/FreeRTOSv202112.00/FreeRTOS/Demo/Common/Minimal/MessageBufferDemo.o: \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Demo/Common/Minimal/MessageBufferDemo.c \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/FreeRTOS.h \
+ FreeRTOSConfig.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcRecorder.h \
+ trcConfig.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcPortDefines.h \
+ trcSnapshotConfig.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcPortDefines.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcHardwarePort.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS-Plus/Source/FreeRTOS-Plus-Trace/Include/trcKernelPort.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/projdefs.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/portable.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/deprecated_definitions.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/portable/ThirdParty/GCC/Posix/portmacro.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/mpu_wrappers.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/task.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/list.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/message_buffer.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Source/include/stream_buffer.h \
+ /home/ale/FreeRTOSv202112.00/FreeRTOS/Demo/Common/include/MessageBufferDemo.h
